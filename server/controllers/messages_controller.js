7c2e622c72e2e8ca896c5b0b185d181cd65d87b7
Phillip Sun <psun.04@gmail.com>
2018-04-21 14:22:11 -0500

    added comments to explain what was going on

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,36 +1,42 @@
+// array to hold all messages
 let messages = []
+// unique id for each message
 let id = 0;
 
 module.exports = {
 
     // read
     read: (req, res) => {
+        // send back status code 200 and messages array
         res.status(200).send(messages)
     },
 
     // create
     create: (req, res) => {
-        // create a message using text 
-        // and time off of the request body
-        // assign a unique id to the message
-        const {
-            text,
-            time
-        } = req.body
+        // create a variable to store the text and time values from request body 
+        const { text, time } = req.body
+        // create a 'message' object using that text and time from request body
         let message = {
             id: id,
             text: text,
             time: time
         }
+        // message object to 'messages' array
         messages.push(message)
+        // increment the 'id' variable that we're using for each messages unique id
         id++
+        // send back a response with the status and messages array
         res.status(201).send(messages)
     },
 
     // update
     update: ( req, res ) => {
+        // get the text from the request body
         const { text } = req.body;
+        // create a variable equal to the request 'id'
         const updateID = req.params.id;
+
+        
         const messageIndex = messages.findIndex( message => message.id == updateID );
         let message = messages[ messageIndex ];
     
@@ -45,11 +51,18 @@ module.exports = {
 
     // delete
     delete: (req, res) => {
+        // create a variable to set the index to when found
         let index = null
+
+        // loop through the 'messages' array
         messages.forEach((message, i) => {
+            // if the message id equals the id from the request
             if(message.id === Number(req.params.id)) index = i;
+            // set the index variable created earlier equal to matching id message
         })
+        // splice using the index variable
         messages.splice(index, 1)
+        // send back response with status and messages 'array'
         res.status(200).send(messages)
     }
 
